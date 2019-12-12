2c804b51e81c8e1cff32ffc96df887f7b84c99c2
Erik <pcaulas3@cebancdea.edu>
2019-12-12 19:06:52 +0100

    ...

--- application/views/dashboard_view.php
+++ application/views/dashboard_view.php
@@ -7,52 +7,43 @@
       integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   </head>
   <body>
-    <div class="container">
-      <div class="row">
-      <nav class="navbar navbar-default">
-          <div class="container-fluid">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Desplegar navegación</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="#">LOGO</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-              <ul class="nav navbar-nav">
+    
+    <div class="container-fluid">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark"">
+        <a class="navbar-brand" href="#">LOGO</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
                 <!-- MENU PARA NIVEL 1 -->
                 <?php if($this->session->userdata('level')==='1'):?>
-                  <li class="active"><a href="<?php echo base_url();?>page/index">Dashboard 1</a></li>
+                  <li class="nav-item active"><a class="nav-link" href="<?php echo base_url();?>page/index">Dashboard 1</a></li>
                   <!-- Link a la función posts dentro del controller Page.php -->
-                  <li><a href="<?php echo base_url();?>page/posts">Posts</a></li>
-                  <li><a href="#">Pages</a></li>
-                  <li><a href="#">Media</a></li>
+                  <li class="nav-item active"><a class="nav-link" href="<?php echo base_url();?>page/posts">Posts</a></li>
+                  <li class="nav-item active"><a class="nav-link" href="#">Pages</a></li>
+                  <li class="nav-item active"><a class="nav-link" href="#">Media</a></li>
                 <!-- MENU PARA NIVEL 2 -->
                 <?php elseif($this->session->userdata('level')==='2'):?>
-                  <li class="active"><a href="<?php echo base_url();?>page/staff">Dashboard Staff</a></li>
-                  <li><a href="#">Pages</a></li>
-                  <li><a href="#">Media</a></li>
+                  <li class="nav-item active"><a class="nav-link" href="<?php echo base_url();?>page/staff">Dashboard Staff</a></li>
+                  <li class="nav-item active"><a class="nav-link" href="#">Pages</a></li>
+                  <li class="nav-item active"><a class="nav-link" href="#">Media</a></li>
                 <!-- MENU PARA NIVEL 3 -->
                 <?php else:?>
-                  <li class="active"><a href="<?php echo base_url();?>page/author">Dashboard Author</a></li>
-                  <li><a href="#">Posts de autores</a></li>
+                  <li class="nav-item active"><a class="nav-link" href="<?php echo base_url();?>page/author">Dashboard Author</a></li>
+                  <li class="nav-item active"><a class="nav-link" href="#">Posts de autores</a></li>
                 <?php endif;?>
               </ul>
-              <ul class="nav navbar-nav navbar-right">
-                <li><a href="<?php echo site_url('login/logout');?>">Cerrar sesión</a></li>
+              <form class="form-inline my-2 my-lg-0">
+                <p class="navbar-text px-2">Bienvenido/a <?php echo $this->session->userdata('username');?>
+                <ul class="nav navbar-nav navbar-right">
+                <li class="nav-item active"><a class="nav-link btn btn-outline-secondary" href="<?php echo site_url('login/logout');?>">Cerrar sesión</a></li>
+              </form>
               </ul>
             </div>
           </div>
         </nav>
- 
-        <div class="jumbotron">
-          <h1>Bienvenido/a <?php echo $this->session->userdata('username');?></h1>
-        </div>
- 
-      </div>
-    </div>
+     </div>
  
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" 
        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
